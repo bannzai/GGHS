4b93f15b8b6a2e083c049bc7df5e4bc508816147
yutailang0119 <muta.yutaro@gmail.com>
2019-06-04 11:52:48 -0700

    Initial Commit

---
+++ GraphGitHubSearch/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GraphGitHubSearch
+//
+//  Created by Yutaro Muta on 2019/06/04.
+//  Copyright © 2019 yutailang0119. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
